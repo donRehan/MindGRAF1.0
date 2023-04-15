805cf97b794e245d541668cfd1e1c92967e930e5
BluBird4 <alhussienrehan@hotmail.com>
2023-04-15 05:47:49 +0200

    ContextSet 90% Done

---
+++ Context.java
@@ -0,0 +1,151 @@
+// Create a method eventually to check for a hypothesis and say its supported in which attitude in which context.
+// Check for keyword later as it contains uncompleted tasks
+// chatGpt means check in asap with chatGPT
+// Rename what needs to be renamed once all the methods have been finished.
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Context implements Serializable{
+    
+    // Attributes
+    // rename to attitudePropositions to be more discreptive 
+    private Hashtable<String, PropositionSet> attitudes;
+    private HashSet<String> names;
+    // Each attitude would have its own hypsBitset.
+    //rename to attitudeHypotheses to be more discreptive
+    private Hashtable<String , BitSet> AttitudesBitset;
+    // Not efficient , Want to make it efficient
+    // rename it to consistentAttitudes to be more discreptive
+    private ArrayList<ArrayList<String>> consistencies;
+
+    //CHANGE THIS FORMAT.
+    protected BitSet getHypsBitset() {
+        return hypsBitset;
+    }
+
+    protected Context() {
+        names = new HashSet<String>();
+        // double check this one too
+        this.attitudes = new Hashtable<>(); 
+        // now bitset  is going to be different for each attitude and hence requires revision too
+        this.AttitudesBitset = new Hashtable<>();
+        this.consistencies = new ArrayList<>();
+    }
+
+    protected Context(String contextName) {
+        this();
+        names.add(contextName);
+    }
+
+    protected Context(Context c) {
+      // To create getAttitudes list method later
+      // Change this too.
+        this.attitudes = c.getAttitudesSet();
+        // to be created. later
+        this.names = c.getNames();
+        // to be created later
+        this.AttitudesBitset = c.getAttitudesBitset();
+    }
+
+    // to create exceptions later ... to add exceptions for wrong attitude etc
+    protected Context(Context c, int hyp, String attitude) throws NotAPropositionNodeException, DuplicatePropositionException, NodeNotFoundInNetworkException {
+        this.names = c.getNames();
+        this.attitudes = c.getAttitudeslist().add(hyp, attitude);
+        BitSet temp = attitudesBitset.get(attitude).clone(); 
+        temp.set(hyp);
+        this.AttitudesBitset.put(attitude, temp);
+    }
+
+    protected Context(String contextName, Context c) {
+        this(c);
+        this.names.add(contextName);
+    }
+
+    protected Context(String contextName, PropositionSet hyps, String attitude) throws NotAPropositionNodeException, NodeNotFoundInNetworkException {
+        this(contextName);
+        this.attitudes.put(attitude, hyps);
+        BitSet temp = new BitSet(); 
+        PropositionSet retrievedPropSet = attitudes.get(attitude);
+        int [] arr = PropositionSet.getPropsSafely(retrievedPropSet);
+        for (int i = 0; i < arr.length; i++){
+             temp.set(arr[i]);
+        }
+        this.AttitudesBitset.put(attitude, temp);
+    } 
+
+    public PropositionSet getHypothesisSet(String attitude) { // of the specific attitude 
+        return this.attitudes.get(attitude);
+    }
+
+    //Will be renamed to getName and have ONLY ONE name per Context
+    protected HashSet<String> getNames() {
+        return names;
+    }
+
+    // to be revised and adjusted as its from AI and all the functions beneath
+    public void addConsistency(String attitudeName, ArrayList<String> consistentAttitudes) {
+        int attitudeIndex = getAttitudeIndex(attitudeName);
+        if (attitudeIndex == -1) {
+            // Attitude not found
+            throw new IllegalArgumentException("Attitude " + attitudeName + " not found.");
+        }
+
+        for (String consistentAttitudeName : consistentAttitudes) {
+            int consistentIndex = getAttitudeIndex(consistentAttitudeName);
+            if (consistentIndex == -1) {
+                // Consistent attitude not found
+                throw new IllegalArgumentException("Attitude " + consistentAttitudeName + " not found.");
+            }
+
+            // Add the consistency relationship to the list
+            ArrayList<String> relationship = new ArrayList<String>();
+            relationship.add(attitudeName);
+            relationship.add(consistentAttitudeName);
+            this.consistencies.add(relationship);
+        }
+    }
+
+    public void removeConsistency(String attitudeName, String consistentAttitudeName) {
+        // Find the relationship in the list and remove it
+        ArrayList<String> relationshipToRemove = null;
+        for (ArrayList<String> relationship : this.consistencies) {
+            if (relationship.get(0).equals(attitudeName) && relationship.get(1).equals(consistentAttitudeName)) {
+                relationshipToRemove = relationship;
+                break;
+            }
+        }
+
+        if (relationshipToRemove != null) {
+            this.consistencies.remove(relationshipToRemove);
+        }
+    }
+
+    public ArrayList<String> getConsistentAttitudes(String attitudeName) {
+        ArrayList<String> consistentAttitudes = new ArrayList<String>();
+
+        // Find all relationships in the list that involve the specified attitude
+        for (ArrayList<String> relationship : this.consistencies) {
+            if (relationship.get(0).equals(attitudeName)) {
+                consistentAttitudes.add(relationship.get(1));
+            }
+        }
+
+        return consistentAttitudes;
+    }
+
+    private int getAttitudeIndex(String attitudeName) {
+        // Find the index of the specified attitude in the list of attitudes
+        int index = -1;
+        int i = 0;
+        for (String name : this.attitudes.keySet()) {
+            if (name.equals(attitudeName)) {
+                index = i;
+                break;
+            }
+            i++;
+        }
+        return index;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,81 @@
-# MindGRAF1.0" 
+# MindGRAF1.0 
 Contexts and Consistency part of the MindGRAF system
-> More details to be added soon ..
+
+NOTE !!
+=> Context Class needs to have one unique name identifier and not a list of names
+
+#### General 
+- [ ] Add consisteny list to controller instead of Context and come up with an efficient implementation.
+- [ ] Address nodes by Ids in Revision System.
+- [ ] Check Context Creations and adding Propositions Attitudes as well as wether the name is unique or not.
+
+## Context Class
+- [ ] isAsserted() a method eventually to check for a hypothesis and say its supported in which attitude in which context. => CH
+- [ ] isSupported. 
+- [ ] allAsserted(). => CH
+//  The three below were of the same style of the previous implementation called JavaSneps and Hence will also be dealt with.
+- [ ] addName.
+- [ ] addNames.
+- [ ] removeName.
+// Correct Part
+- [ ] getName.
+- [ ] Function for unique name or is it given by the user ?.
+
+## ContextSet
+- Double check rules for new implementation before proceeding .
+- [x] ContextSet
+- [x] ContextSet(String name)
+- [x] ContextSet(Context context)
+- [x] getContext(String name)
+- [x] remove(String name)
+- [x] add(context context)
+- [ ] Fix Consturctors to only have one string as the name that identifies the Context
+
+## Controller Class
+> To be added later.
+
+## ContextSet
+Document your code here for your bachelor document.
+
+### Methods
+
+// Insert Screenshot of the method. 
+Here remove gets the Context by its name and removes it from the ContextSet.
+
+### removing and adding a Context
+```java
+// given a Context name it removes it from the ContextSet
+    public boolean remove(String name) {
+        return contexts.remove(name) != null;
+    }
+```
+### Constructing  contextSets
+ ``` java
+//Empty Constructor
+    public ContextSet() {
+        contexts = new Hashtable<String, Context>();
+    }
+
+//Constructor with name of an empty Context
+    public ContextSet(String name) {
+        this();
+        this.contexts.put(name, new Context(name));
+    }
+
+// Constructor with a Context
+	public ContextSet(Context context) {
+        this();
+        this.add(context);
+    }
+```
+
+### Adding a Context
+```java
+
+//Returns a Context Object given its name.
+    public Context getContext(String name) {
+        return contexts.get(name);
+    }
+```
+
+> This section is not finished yet !
